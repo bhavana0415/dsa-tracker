f1c449d1ce0694fd8d2ff1759a374608942cb350
BHAVANA MANASWINI <bhavanamanaswi@gmail.com>
2024-03-06 23:36:33 +0530

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,34 +27,18 @@ export default function Home() {
     fetchData();
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(questions);
-  },[questions]);
+  }, [questions]);
 
 
   return (
     <main>
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+      <div
+        style={{ backgroundImage: 'url(/coder.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
+        className="w-[500px] h-[400px]">
+      </div>
+
     </main>
   );
 }
