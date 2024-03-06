522023b40196d68af84195f748718dfc3a334012
BHAVANA MANASWINI <bhavanamanaswi@gmail.com>
2024-03-06 21:35:34 +0530

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from "./GlobalRedux/store";
 import { fetchQuestionsAsync } from "./GlobalRedux/FetchData/fetchSlice";
 import { useEffect } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function Home() {
   const questions = useSelector((state: RootState) => state.fetchData.currentQuestions);
@@ -27,6 +33,28 @@ export default function Home() {
 
 
   return (
-    <main>Welcome to DSA Tracker</main>
+    <main>
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    </main>
   );
 }
